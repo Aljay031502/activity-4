b6b48bb747c4d9589eddbc076d6dbda6c777af5e
Aljay031502 <112836615+Aljay031502@users.noreply.github.com>
2022-10-21 08:37:11 +0800

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,28 @@
+var arr1=[30, 15, 12, 21, 19];
+var arr2=[];
+var min=arr1[0];
+var pos;
+var max=arr1[0];
+for (i=0; i<arr1.length; i++)
+{
+        if (max<arr1[i]) max=arr1[i];
+}
+
+for (var i=0;i<arr1.length;i++)
+{
+        for (var j=0;j<arr1.length;j++)
+        {
+                if (arr1[j]!="x")
+                {
+                        if (min>arr1[j]) 
+                        {
+                                min=arr1[j];
+                                pos=j;
+                        }
+                }
+        }
+        arr2[i]=min;
+        arr1[pos]="x";
+        min=max;
+}
+console.log(arr2);
